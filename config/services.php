f9f9fc53d0eddd9c2afa46b9adc0cd8fc57f7186
Kenth Alexis Osila <kenthosila@gmail.com>
2026-09-23 12:30:42 +0800

    chore: scaffold laravel project foundation

---
+++ config/services.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'github' => [
+        'token' => env('GITHUB_TOKEN'),
+        'graphql_url' => 'https://api.github.com/graphql',
+        'cache_ttl' => env('GITHUB_CACHE_TTL', 1800), // 30 minutes
+        'enable_api' => env('ENABLE_API', false),
+        'repo_url' => env('REPO_URL', 'https://github.com/codebykenth/github-readme-generator'),
+    ],
+
+];
